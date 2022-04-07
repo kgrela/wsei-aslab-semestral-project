04a2ac2f97648cf43a126fcc3d716d597f85dc26
kacper.grela <kacper.grela@PK2-16>
2022-04-07 19:16:28 +0200

    Dodaj pliki projektów.

---
+++ wsei-aslab-semestral-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wsei_aslab_semestral_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
